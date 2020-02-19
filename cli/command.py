40f16e447f2e17ffe213f072eb4dc8d70f012a75
guoxudong <guoxudong@keking.cn>
2020-02-19 17:02:12 +0800

    fix base64

--- cli/command.py
+++ cli/command.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 # Author: guoxudong
-import base64
 from argparse import ArgumentParser
 from os import getenv
 
@@ -42,9 +41,7 @@ def main():
     )
     args = parser.parse_args()
     try:
-        access_key_id = base64.b64encode(args.access_key_id.encode("utf-8"))
-        access_secret = base64.b64encode(args.access_secret.encode("utf-8"))
-        specs = load_arg(str(access_key_id), str(access_secret), args.region_id)
+        specs = load_arg(args.access_key_id, args.access_secret, args.region_id)
         handle_k8s(specs)
         print('Success generating json configuration!')
     except KeyboardInterrupt:
